125ea4fa4d4147fee1b2e923bce7e64e5f9b1a69
Dmitriy Sorokin <dimsa@inbox.ru>
2025-08-12 17:10:48 +0700

    Additional fix

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -204,6 +204,7 @@ namespace Aspose.GIS.Examples.CSharp
         {
             string assemblyPath = Assembly.GetExecutingAssembly().Location;
             string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+            string testData = Path.Combine(assemblyDirectory, "..", "..", "Data");
             return assemblyDirectory;
         }
 
